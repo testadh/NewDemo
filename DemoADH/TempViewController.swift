1dbef49d015753776fa39f48f8ea8a4977a7d0d6
AdityaBirlaHealth Common <abhcommon@AdityaBirlaHealths-Mac-mini.local>
2017-08-21 17:41:29 +0530

    second

--- DemoADH/TempViewController.swift
+++ DemoADH/TempViewController.swift
@@ -12,7 +12,7 @@ class TempViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        print("dsdsd")
         // Do any additional setup after loading the view.
     }
 
